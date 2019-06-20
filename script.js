3072b1344b91014771f076a89a9bd899d07f2886
Ayush Acharya <ayush.acharya2104@gmail.com>
2019-06-20 15:24:28 +0545

    added forum

---
+++ script.js
@@ -0,0 +1,23 @@
+function render(data){
+    var html = "<div class='commentBox'><div class='leftPaneImg'><img src='https://placeholdit.igmix.net/~text?w=100&h=100'/></div> <div class='rightPanel'><span>"+data.Queries+"</span></div>";
+    $('#form').append(html);
+
+}
+
+$(document).ready(function(){
+    //ready all the html components.
+    var comment = [
+        { "name": "Ayush Acharya", "Queries":"Hey There Can we be Friends?"}
+    ];
+    for(var i=0; i<comment.length;i++){
+      render(comment[i]);
+    }
+    $('#addComment').click(function(){
+        var addobj ={
+            "Queries":$('#Queries').val()
+        };
+        console.log(addobj);
+        comment.push(addobj);
+        render(addobj); 
+    });
+});
