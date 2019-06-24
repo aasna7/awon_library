028a54c802dcda0991dbdd1f7bd585e7ca226cd8
Ayush Acharya <ayush.acharya2104@gmail.com>
2019-06-24 14:59:42 +0545

    forum is added

--- Forum.js
+++ Forum.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname,)));
 // app.get('/',(req, res) =>{
 //     res.sendFile(path.join(__dirname, 'Aboutus.html'));
 // })
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5550;
 
 app.listen(PORT,()=> console.log(`Forum Started on Port ${PORT}`));
 //Create a function name render
